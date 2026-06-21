4a525213cf63fcd7d61cf5b19509fda5eaf2325e
Krizunoar <i202511798@cibertec.edu.pe>
2026-06-21 00:23:54 -0500

    Cambio 1
    Conectar aplicacion a base de datos Azure MySQL

--- src/main/java/com/torneo/repository/UsuarioRepository.java
+++ src/main/java/com/torneo/repository/UsuarioRepository.java
@@ -2,7 +2,8 @@ package com.torneo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.torneo.entity.Usuario;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+	Optional<Usuario> findByUsername(String username);
 }
---
+++ src/main/java/com/torneo/security/UsuarioDetails.java
@@ -0,0 +1,59 @@
+package com.torneo.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.torneo.entity.Usuario;
+
+public class UsuarioDetails implements UserDetails {
+
+    private final Usuario usuario;
+
+    public UsuarioDetails(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String rol = "ROLE_" + usuario.getRol().getNombre();
+        return List.of(new SimpleGrantedAuthority(rol));
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return usuario.getEstado() != null && usuario.getEstado();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+}
